094ac59506b603053e1b2909f0ac15804134affa
George Antonov <guljash.main@gmail.com>
2020-02-10 02:19:41 +0300

    v2

--- src/components/Slider/Slide/Slide.jsx
+++ src/components/Slider/Slide/Slide.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import arrow from './arrow.svg'
+import styles from './Slide.module.css'
+
 
 const Slide = (props) => {
+
     return (
-        <div className="slide">
-            <img onClick={props.prev} className="prev" src={arrow} alt="prev"/>
-            {props.val}
-            <img onClick={props.next} className="next" src={arrow} alt="next"/>  
+        <div style={{left: props.move + '%'}} className={styles.slide}>
+            <div className={styles.one}>Powered by</div>
+            <div className={styles.two}>Bootstrap</div>
+            <div className={styles.three}>React.js</div>
         </div>
     )
 }
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Slide from './Slide/Slide';
+import arrow from './arrow.svg'
 
 class Slider extends React.Component {
     constructor() {
         super();
         this.state = {
             slides: ['Один', 'Два', 'Три'],
-            index: 0
+            index: 0,
+            move: 0,
+            interval: 5000,
         }
     }
 
     componentDidMount() {
         this.timerID = setInterval(
             () => this.nextHandler(),
-            4000
+            this.state.interval
         );
     }
 
@@ -23,29 +26,33 @@ class Slider extends React.Component {
 
     nextHandler = () => {
         let i = this.state.index;
-        console.log(i)
         i++;
         if (i >= this.state.slides.length) {
             i = 0;
         }
-        this.setState({ index: i });
+        this.setState({ index: i, move: -i * 100 });
     }
 
     prevHandler = () => {
         let i = this.state.index;
-        i--
+        i--;
         if (i < 0) {
             i = this.state.slides.length;
             i--;
         }
-        this.setState({ index: i });
+        this.setState({ index: i, move: -i * 100 });
     }
 
     render() {
         return (
-            <div className="row">
+            <div>
+                <div className="row">
+                    <div className="col-sm slider-header">Галерея</div>
+                </div>
                 <div className="col-sm slider">
-                    <Slide val={this.state.slides[this.state.index]} prev={this.prevHandler} next={this.nextHandler} />
+                    <img onClick={this.prevHandler} className="prev" src={arrow} alt="prev" />
+                    <Slide move={this.state.move} />
+                    <img onClick={this.nextHandler} className="next" src={arrow} alt="next" />
                 </div>
             </div>
         )
